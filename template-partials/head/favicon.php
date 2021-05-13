09305cf7372240941dace41a60e38f245a901ea7
Patrick Leisinger <patrick.leisinger@gmail.com>
2021-05-13 11:22:16 +0200

    [UPDATE] add final fixes

--- template-partials/head/favicon.php
+++ template-partials/head/favicon.php
@@ -10,9 +10,9 @@
 <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/favicon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/favicon/favicon-16x16.png">
-<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/favicon/site.webmanifest">
-<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/favicon/safari-pinned-tab.svg" color="#cf3c3b">
-<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon/favicon.ico">
+<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/favicon/site.webmanifest">
+<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon/safari-pinned-tab.svg" color="#cf3c3b">
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon/favicon.ico">
 <meta name="apple-mobile-web-app-title" content="SoaPatrick">
 <meta name="application-name" content="SoaPatrick">
 <meta name="msapplication-TileColor" content="#ffffff">
